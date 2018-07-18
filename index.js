d3e8b7781c02ef4f6323e5191985401f63d9864b
Simon A Bull <simonbull1@gmail.com>
2018-07-18 17:56:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ function add(a,b){
   console.log (a + b)
   return (a + b)
 }
+
+function subtract(a,b){
+  console.log (a - b)
+  return (a - b)
+}
+
+function multiply(a,b){
+  console.log (a * b)
+  return (a * b)
+}
+
+function divide(a,b){
+  console.log (a / b)
+  return (a / b)
+}
