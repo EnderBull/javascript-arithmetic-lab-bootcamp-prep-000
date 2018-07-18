2197ae8ca1d6751b676eef9532c06dbbcfc8f7b1
Simon A Bull <simonbull1@gmail.com>
2018-07-18 18:27:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,10 +31,11 @@ function dec(n){
 }
 
 function makeInt(n){
-  parseInt(n,10)
+  parseInt(n, 10)
   return (n)
 }
 
 function preserveDecimal(n){
-  parseFloat(n)
+  parseFloat(n, 10)
+  return(n)
 }
